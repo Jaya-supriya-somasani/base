4b3d032134eddfc6e201ac717e665712430cab67
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-01-30 23:08:47 +0530

    base fragment is created

---
+++ base/src/main/java/com/example/base/BaseFragment.kt
@@ -0,0 +1,13 @@
+package com.example.base
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Base Fragment of all the fragments
+ *
+ * @param <VM>
+ * @param <Binding>
+</Binding></VM> */
+abstract class BaseFragment : Fragment() {
+
+}
