ae5cc0270ac2261bcab17a277ccc961f19094664
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-03-22 11:37:45 +0530

    Base Adapter created

---
+++ base/src/main/java/com/example/base/BaseHolder.kt
@@ -0,0 +1,13 @@
+package com.example.base
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseHolder<Item>(view: View) : RecyclerView.ViewHolder(view) {
+
+    abstract fun onBind(item: Item)
+
+    open fun onRecycled() {
+        // override
+    }
+
+}
